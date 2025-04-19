77cc77a78d31b4382cc0c80daea7ffe79e44f11b
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-04-19 07:42:56 +0530

    Add files via upload

---
+++ SecondServlet.java
@@ -0,0 +1,39 @@
+package in.abc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/test2")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//collect request parameter
+		
+		String pcost = request.getParameter("pcost");
+		String pquantity = request.getParameter("pquantity");
+		
+		//Create a cookie as per the requirements(inputs)
+		Cookie c3 = new Cookie("pcost", pcost);
+		Cookie c4 = new Cookie("pquantity", pquantity);
+		
+		//send the cookie object through response
+		response.addCookie(c3);
+		response.addCookie(c4);
+		
+		//forward the response to form3.html
+		RequestDispatcher rd = request.getRequestDispatcher("/form3.html");
+		rd.forward(request, response);
+		
+	}
+
+}
